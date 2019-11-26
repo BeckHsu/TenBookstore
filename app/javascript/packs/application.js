8fa34cedc50a0a0b50d57b2e0e4d6dff1f50d013
beckhsu <justin7610@gmail.com>
2019-11-26 10:19:01 +0800

    資料夾更新

--- app/javascript/packs/application.js
+++
@@ -1,9 +0,0 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-// import 'bulma/css/bulma.css'
-// import '../stylesheets/index.js'
-import '../stylesheets'
-import '../scripts'
