15f0cbdc9f644fe66bad5043e92fe2dfede87fae
beckhsu <justin7610@gmail.com>
2019-12-09 12:13:59 +0800

    googleLogIn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users,controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks' 
+  }
   resources :books, except: [:new, :create]   #,path: 'products'  # 8 routes => 7 actions
   root 'books#index'
 
